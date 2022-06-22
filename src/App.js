c5fcc675a78ec0c4c7f99b244e4cb59476b442eb
Tabalin Nikolay <Zerru_Kun@yahoo.com>
2022-06-22 13:44:27 +0300

    Базовая стилизация.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import React from "react";
+import styles from "./App.module.css";
+
 function App() {
   return (
-    <div className="App">
-      <h1>All is ok</h1>
+    <div className={styles.appContainer}>
+      <div className={styles.headerItem}>header</div>
+      <div className={styles.sidebarItem}>sidebar</div>
+      <div className={styles.mainItem}>main</div>
+      <div className={styles.footerItem}>footer</div>
     </div>
   );
 }
