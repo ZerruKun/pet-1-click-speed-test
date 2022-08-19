a5d47c2e3ba8f8fe69337bd8939cfcb1a968fe82
Tabalin Nikolay <Zerru_Kun@yahoo.com>
2022-08-19 14:38:00 +0300

    Базовая разметка.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-
+import React from "react";
+import Header from "./components/header/Header"
+import Footer from "./components/footer/Footer"
+import Sidebar from "./components/sidebar/Sidebar"
 
 function App() {
   return (
     <div>
+      <Header />
+      <Sidebar />
 
+      <Footer />
     </div>
   );
 }
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+      <h1>Я Footer</h1>
+    </div>
+  )
+}
+
+export default Footer
